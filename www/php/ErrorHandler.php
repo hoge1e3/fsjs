bcbf9ec13f8fe3a458cda019daab163f4aa30adf
hoge1e3 <hoge1e3@gmail.com>
2016-08-10 15:01:02 +0900

    Permission

--- www/php/ErrorHandler.php
+++ www/php/ErrorHandler.php
@@ -1,7 +1,32 @@
 <?php
+if (!function_exists('http_response_code'))
+{
+    function http_response_code($newcode = NULL)
+    {
+        static $code = 200;
+        if($newcode !== NULL)
+        {
+            header('X-PHP-Response-Code: '.$newcode, true, $newcode);
+            if(!headers_sent())
+                $code = $newcode;
+        }       
+        return $code;
+    }
+}
 function h_err($errno, $errstr, $errfile, $errline) {
-    echo "ERR $errno $errstr $errfile:$errline";
-    exit(1);
+    http_response_code(500);
+    $buf="\n";
+    if (function_exists('debug_backtrace')) {
+        $tr=debug_backtrace();
+        foreach ($tr as $t) {
+            if (isset($t["function"]) && 
+            isset($t["file"]) && isset($t["line"])) {
+                $buf.="at ".$t["function"]." ".$t["file"].":".$t["line"]."\n";
+            }
+        }
+    }
+    die ("SERVER ERROR!\n$errno $errstr $errfile:$errline$buf\nSERVER ERROR END!");
+    //exit(1);
 }
 set_error_handler("h_err");
 ?>
